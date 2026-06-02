5920445bbb9d765860ada2da34310e2f4ecdb5c5
Reneoswaldo <pineapplexport.bogota@gmail.com>
2026-06-02 16:50:47 -0500

    Proyecto React funcionando

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Little Lemon</h1>
+      <p>Proyecto funcionando correctamente</p>
+    </div>
+  );
+}
+
+export default App;
